3f78acc96dfe19dd270430140b6ed95fbd9b422f
Aundrea Gordon <aundreagordon@Aundreas-Mac-mini.local>
2026-06-29 21:12:20 -0500

    Add GitHub Actions OIDC workflow for Flask CI

---
+++ terraform/github-oidc.tf
@@ -0,0 +1,63 @@
+resource "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+
+  client_id_list = [
+    "sts.amazonaws.com"
+  ]
+
+  thumbprint_list = [
+    "6938fd4d98bab03faadb97b34396831e3780aea1"
+  ]
+}
+resource "aws_iam_role" "github_actions_ecr_push" {
+  name = "github-actions-ecr-push-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.github.arn
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+          }
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" = "repo:aundreagordon/eks-cicd-pipeline:*"
+          }
+        }
+      }
+    ]
+  })
+}
+resource "aws_iam_role_policy" "github_actions_ecr_push" {
+  name = "github-actions-ecr-push-policy"
+  role = aws_iam_role.github_actions_ecr_push.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ecr:GetAuthorizationToken"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:CompleteLayerUpload",
+          "ecr:InitiateLayerUpload",
+          "ecr:PutImage",
+          "ecr:UploadLayerPart"
+        ]
+        Resource = aws_ecr_repository.app.arn
+      }
+    ]
+  })
+}
